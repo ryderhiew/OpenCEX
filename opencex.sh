280aa041e5661f507c318e70c4b31533a9d9b7b2
Ryder Blaise Hiew <ryderhiew@Ryders-MacBook-Pro.local>
2025-01-28 11:37:23 +0800

    ryder repos

--- opencex.sh
+++ opencex.sh
@@ -18,10 +18,10 @@ fi
 
 mkdir /app/opencex -p
 cd /app/opencex || exit
-git clone  https://github.com/Polygant/OpenCEX-backend.git ./backend
-git clone  https://github.com/Polygant/OpenCEX-frontend.git ./frontend
-git clone  https://github.com/Polygant/OpenCEX-static.git ./nuxt
-git clone  https://github.com/Polygant/OpenCEX-JS-admin.git ./admin
+git clone  https://github.com/ryderhiew/OpenCEX-backend ./backend
+git clone  https://github.com/ryderhiew/OpenCEX-frontend.git ./frontend
+git clone  https://github.com/ryderhiew/OpenCEX-static.git ./nuxt
+git clone  https://github.com/ryderhiew/OpenCEX-JS-admin.git ./admin
 
 echo "`cat <<YOLLOPUKKI
 
